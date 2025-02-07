7b1beee53c820d9d390bde8179c568701d317bcb
Panzershrekk <32634884+Panzershrekk@users.noreply.github.com>
2025-02-07 14:04:34 +0100

    Changing parameters

---
+++ Source/ProjectStillBlue/Public/SB_CharacterMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SB_CharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSTILLBLUE_API USB_CharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	float Test;
+};
